bd3ef9d7e994c2c9b8bdca4eb23d38949a530e50
Aleksandr Ovchinnikov <alexander.ovchinnikoff@gmail.com>
2026-08-09 13:08:11 +0300

    Update config.py

--- api/config.py
+++ api/config.py
@@ -1,3 +1,4 @@
+# config.py
 URL_SERVICE = "https://qa-scooter.praktikum-services.ru/"
 CREATE_NEW_ORDER = "api/v1/orders" #путь к API-методу для создания нового заказа
 GET_ORDER_INFO = "api/v1/orders/track" #путь к API-методу для получения информации о заказе по трекномеру
